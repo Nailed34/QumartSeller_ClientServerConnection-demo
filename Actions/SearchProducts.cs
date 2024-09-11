dd3c1912743e7fbec36291d89ae85f14c0ac99c9
Nailed34 <ilianik34@mail.ru>
2024-09-12 02:30:37 +0300

    add: classes for search requests

---
+++ Actions/SearchProducts.cs
@@ -0,0 +1,29 @@
+﻿/**
+ * @QumartSeller_ClientServerConnection
+ * https://github.com/Nailed34/QumartSeller_ClientServerConnection-demo.git
+ *
+ * Copyright (c) 2024 https://github.com/Nailed34
+ * Released under the MIT license
+ */
+
+namespace ClientServerConnection.Actions
+{
+    /// <summary>
+    /// Class for search products in range
+    /// </summary>
+    public class InSearchProducts
+    {
+        public string SearchRequest { get; set; } = "";
+        public int FirstIndex { get; set; } = 0;
+        public int LastIndex { get; set; } = 0;
+    }
+
+    /// <summary>
+    /// Server response, contains serialized products and found products count
+    /// </summary>
+    public class OutSearchProducts
+    {
+        public List<string> FoundProducts { get; set; } = new List<string>();
+        public int FoundProductsCount { get; set; } = 0;
+    }
+}
